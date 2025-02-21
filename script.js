e14ba0190a2c3152c4d4e60882b974cd9f9f4574
KhanhQuynh299 <khanhheo262@gmail.com>
2025-02-21 10:17:52 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login()
+{
+  let u = username.value
+  let p = pw.value
+  if (u=="Dacat" && p="Dacat2909@%")
+  {
+    window.open("home.html")
+    document.getElementByteId(compiler).style.display="block"
+  }
+    
